02dbafaa8c0dc39eb32d325ceebb9abf14c21b9a
Windows-10 <Windows-10@DESKTOP-AUHNTHL>
2022-11-12 11:41:54 +0530

    Comiit1-Test1

---
+++ src/gitDemoPack/Test1.java
@@ -0,0 +1,12 @@
+package gitDemoPack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String s="Vaibhav";
+        //1.Using inbuilt method length() of string.
+        System.out.println(s.length());
+
+	}
+
+}
